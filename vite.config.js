f0aedc46b9192017766e4c681d50e4dd01b04eb2
Leosparroww <leosparrow.yota@gmail.com>
2023-08-03 00:58:33 +0630

    e

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
-
+import "./bootstrap";
+import "../css/app.css";
 export default defineConfig({
     plugins: [
         laravel({
