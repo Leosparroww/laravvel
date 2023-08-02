157e4d0be54f5e06e65554f1a94fbd18e007a109
Leosparroww <leosparrow.yota@gmail.com>
2023-08-03 01:03:19 +0630

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
 import "./bootstrap";
 import "../css/app.css";
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**',
-            ],
-        }),
-    ],
+    plugins: [laravel(["resources/css/app.css", "resources/js/app.js"])],
 });
